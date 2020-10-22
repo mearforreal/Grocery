9f05fd4a087c68ee2af0c2b5e5b7fbc3165e9056
Ereke013 <meirerbol@gmail.com>
2020-10-22 11:07:01 +0600

    This is my first commit

---
+++ js/main.js
@@ -0,0 +1,33 @@
+// First Slider
+$(".slider-one")
+    .not(".slick-intialized")
+    .slick({
+        autoplay: true,
+        autoplaySpeed: 3000,
+        dots: true,
+        prevArrow: ".site-slider .slider-btn .prev",
+        nextArrow: ".site-slider .slider-btn .next"
+    });
+
+// Second Slider
+$(".slider-two")
+    .not(".slick-intialized")
+    .slick({
+        prevArrow: ".site-slider-two .prev",
+        nextArrow: ".site-slider-two .next",
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplaySpeed: 3000
+    });
+
+// Third Slider
+$(".slider-three")
+    .not(".slick-intialized")
+    .slick({
+        prevArrow: ".site-slider-three .prev",
+        nextArrow: ".site-slider-three .next",
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplaySpeed: 3000,
+        infinite: true
+    });
